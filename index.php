faa6976c5054e7181652b80f4eaa907a7bf75b87
Kristjan Vool <vool.kristjan@gmail.com>
2015-09-23 23:02:36 +0300

    asd

--- index.php
+++ index.php
@@ -58,7 +58,7 @@ var helper = (function() {
 			$('#authResult').append(' ' + field + ': ' + authResult[field] + '<br/>');
 		}
 		if (authResult.isSignedIn.get()) {
-			window.location = "localhost/landing.php"
+			window.location.replace("http://stackoverflow.com");
 			$('#gConnect').hide();
 			helper.profile();
 		} else if (authResult['error'] ||
